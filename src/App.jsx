d6ac88d39e7deedfbf75e7ecf8aba759be0579e8
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-28 21:01:58 +0530

    DatePicker and RangePicker from DatePicker is practiced

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
+import { useState } from "react";
+import { DatePicker } from "antd";
+import moment from "moment";
+
+const { RangePicker } = DatePicker;
+
 const App = () => {
+  console.log('Component Rendered!!!');
+  const [singelDates, setSingleDates] = useState();
+  const [rangeDates, setRangeDates] = useState([]);
+  console.log('singelDates', singelDates);
+  console.log('rangeDates', rangeDates);
   return (
-    <div>App</div>
+    <div>
+      <div style={{ margin: "20px" }}>
+        <DatePicker
+          onChange={(value) => {
+            const getValue = moment(value).format('DD-MM-YYYY')
+            setSingleDates(getValue);
+          }}
+        />
+      </div>
+      <div style={{ margin: "20px" }}>
+        <RangePicker
+          onChange={(value) => {
+            setRangeDates(value.map((item) => {
+              return moment(item).format('DD-MM-YYYY');
+            }));
+          }}
+        />
+      </div>
+    </div>
   )
 }
 
